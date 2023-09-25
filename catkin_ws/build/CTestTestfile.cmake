6096cfd9ac425e63033a82e19caac35c61fbcb3c
ZNeslisah <115110468+ZNeslisah@users.noreply.github.com>
2023-09-25 19:43:20 +0200

    Add files via upload

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/neslisah/catkin_ws/src
+# Build directory: /home/neslisah/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("control_package")
+subdirs("perception_package")
+subdirs("robot_navigation")
+subdirs("my_robot_controller")
+subdirs("neslisah_ros_package")
