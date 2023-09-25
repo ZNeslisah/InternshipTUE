197b732220c86bb7da830cd45da1ad03f64dd23c
ZNeslisah <115110468+ZNeslisah@users.noreply.github.com>
2023-09-25 19:42:42 +0200

    Add files via upload

---
+++ catkin_ws/build/perception_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/neslisah/catkin_ws/src/perception_package
+# Build directory: /home/neslisah/catkin_ws/build/perception_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/robot_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/neslisah/catkin_ws/src/robot_navigation
+# Build directory: /home/neslisah/catkin_ws/build/robot_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
